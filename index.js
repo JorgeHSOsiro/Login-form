f65bc4a85ec349697e0d5b98cc9a247f7cc3f6a3
jorgehsosiro <jojimurakabe@gmail.com>
2021-06-25 11:39:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const switchCtn = document.querySelector('#switch-cnt');
+const switchC1 = document.querySelector('#switch-c1');
+const switchC2 = document.querySelector('#switch-c2');
+const switchCircle = document.querySelectorAll('.switch_circle');
+const switchBtn = document.querySelectorAll('.switch-btn');
+const aContainer = document.querySelector('.a-container');
+const bContainer = document.querySelector('.b-container');
+const allButtons = document.querySelectorAll('.submit');
+
+const getButtons = (e) => {
+    e.preventDefault();
+}
+const changeForm = () => {
+    switchCtn.classList.add('is-gx');
+    setTimeout(function() {
+        switchBtn.classList.remove('is-gx');
+    }, 1500);
+
+    switchCtn.classList.toggle('is-txr');
+    switchCircle[0].classList.toggle('is-txr');
+    switchCircle[1].classList.toggle('is-txr');
+
+    switchC1.classList.toggle('is-hidden');
+    switchC2.classList.toggle('is-hidden');
+    aContainer.classList.toggle('is-txl');
+    bContainer.classList.toggle('is-txl');
+    bContainer.classList.toggle('is-z200');
+}
+
+
+const mainF = (e) => {
+    for (let i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener('click', getButtons)
+        
+    }
+    for (let i = 0; i < switchBtn.length; i++) {
+        switchBtn[i].addEventListener('click', changeForm)
+        
+    }
+}
+
+window.addEventListener('load', mainF);
